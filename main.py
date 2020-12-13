25b4d456825fcb0a1501f36035896d9ead03c6d8
DaniG2 <garciagiraldo9@gmail.com>
2020-12-13 00:14:36 -0500

    Mas y mas

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
     "http://localhost", "http://localhost:8080", "http://localhost:8080", 
-    "http://ensayo-cajero-app.herokuapp.com", "http://ensayo-cajero-app.herokuapp.com",
-    "http://127.0.0.1:8080", "http://ensayo-cajero-api.herokuapp.com"
+    "http://ensayo-cajero-app.herokuapp.com", "http://127.0.0.1:8080", 
+    "http://ensayo-cajero-api.herokuapp.com/"
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
