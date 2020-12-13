999fd19399685b6e124132e168a3fbd83c27f623
DaniG2 <garciagiraldo9@gmail.com>
2020-12-13 00:39:48 -0500

    Mas y mas

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
     "http://localhost", "http://localhost:8080", "http://localhost:8000",
     "http://ensayo-cajero-app.herokuapp.com/", "http://127.0.0.1:8080", 
-    "http://ensayo-cajero-api.herokuapp.com"
+    "http://ensayo-cajero-api.herokuapp.com", "https://ensayo-cajero-app.herokuapp.com"
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
